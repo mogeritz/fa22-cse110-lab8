ef20c552784043b010e8d8eb5ab19bf393d08178
mogeritz <moge.world.kanal@gmail.com>
2022-11-19 19:40:41 -0800

    Finished Lab 8

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # Lab 8 - Starter
+## Answer to question 1:
+I would choose 1), because that removes the factor of human error, enforces constant running of tests, and, as they are automated anyway, wont cost much in terms of complexity.
+This would ensure the highest quality of code within the team repo
+## Answer to question 2:
+No, because this is not directly related to user actions.
+## Answer to question 3:
+Definitely not, as this is something more related to e2e testing, as it involves multiple components playing together.
+## Answer to question 4:
+It can be done, assuming the send function is accessible as javascript, and is designed/should be designed to handle this case. If it isn't, this would be more related to e2e testing.
+
+## Written by Moritz Wagner
--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -3,3 +3,82 @@
 const functions = require('../code-to-unit-test/unit-test-me.js');
 
 // TODO - Part 2
+test('Test my phone number', () => {
+    expect(functions.isPhoneNumber("(760)655-8419")).toBe(true);
+  });
+
+test('Test another phone number', () => {
+    expect(functions.isPhoneNumber("+(420)123-6969")).toBe(true);
+});
+
+test('Test german phone number', () => {
+    expect(functions.isPhoneNumber("+4917639244647")).toBe(false);
+});
+
+test('Test american phone number in wrong format', () => {
+    expect(functions.isPhoneNumber("+17606559419")).toBe(false);
+});
+
+test('project mail', () => {
+    expect(functions.isEmail("ursula4triton@gmail.com")).toBe(true);
+});
+
+test('my mail', () => {
+    expect(functions.isEmail("moritzswagner@gmail.com")).toBe(true);
+});
+
+test('fake TLD mail', () => {
+    expect(functions.isEmail("ursula4triton@gmail.yeet")).toBe(false);
+});
+
+test('no domain mail', () => {
+    expect(functions.isEmail("ursula4triton@yeet")).toBe(false);
+});
+
+test('random strong password 1', () => {
+    expect(functions.isStrongPassword("iodAWGbj2Ikk")).toBe(true);
+});
+
+test('random strong password 2', () => {
+    expect(functions.isStrongPassword("opjAHPodw2")).toBe(true);
+});
+
+test('weak password 1', () => {
+    expect(functions.isStrongPassword("12345678")).toBe(false);
+});
+
+test('weak password 2', () => {
+    expect(functions.isStrongPassword("1password")).toBe(false);
+});
+
+test('9/11 date', () => {
+    expect(functions.isDate("9/11/2002")).toBe(true);
+});
+
+test('birthday date', () => {
+    expect(functions.isDate("1/18/2002")).toBe(true);
+});
+
+test('incorrect birthday date', () => {
+    expect(functions.isDate("181/1/2002")).toBe(false);
+});
+
+test('missing year date', () => {
+    expect(functions.isDate("18/1")).toBe(false);
+});
+
+test('nice pink color', () => {
+    expect(functions.isHexColor("#ff3399")).toBe(true);
+});
+
+test('black color', () => {
+    expect(functions.isHexColor("#000000")).toBe(true);
+});
+
+test('missing digit color', () => {
+    expect(functions.isHexColor("#00000")).toBe(false);
+});
+
+test('wrong letter color', () => {
+    expect(functions.isHexColor("#0g000")).toBe(false);
+});
